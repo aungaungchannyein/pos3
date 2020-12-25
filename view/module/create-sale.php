6d16bee4f34713850f2a95542f5177aa85b2b3e8
ChanNyein AungAung <aungaung.channyein@gmail.com>
2020-12-25 22:53:28 +0630

    create Sale

--- view/module/create-sale.php
+++ view/module/create-sale.php
@@ -23,8 +23,9 @@
         <div class="col-lg-5 col-xs-12">
            <div class="box box-success">
              <div class="box-header with-border"></div>
+             <form role="form" method="post">
              <div class=box-body>
-               <form role="form" method="post">
+               
                  <div class="box">
                   <div class="form-group">
                     <div class="input-group">
@@ -61,25 +62,104 @@
                     </div>
                       <div class="col-xs-3" style="padding-left:0px">
                       <div class="input-group">
-                        <input type="number" class="form-control" name="newProductPrice" id="newProductPrice" placeholder="0000000" readonly required>
                         <span class="input-group-addon"><i class="ion ion-social-usd"></i></span>
+                        <input type="number" class="form-control" name="newProductPrice" id="newProductPrice" placeholder="0000000" readonly required>
+                        
                       </div>
                     </div>
                   </div>
                   <button type="button" class="btn btn-default hidden-lg">Add product</button>
                   <hr>
-                  <div class="row"></div>
+                  <div class="row">
+                    <div class="col-xs-8 pull-right">
+                      <table class="table">
+                        <thead>
+                          <tr>
+                            <th>Tax</th>
+                            <th>Total</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td style="width:50%">
+                              <div class="input-group">
+                                <input type="number" class="form-control" name="newTaxSale" id="newTaxSale" placeholder="0" readonly required>
+                                <span class="input-group-addon"><i class="fa fa-percent"></i></span>
+                              </div>
+                            </td>
+                              <td style="width:50%">
+                              <div class="input-group">
+                                <span class="input-group-addon"><i class="ion ion-social-usd"></i></span>
+                                <input type="number" class="form-control" name="newSaleTotal" id="newSaleTotal" placeholder="0" readonly required>
+                                
+                              </div>
+                            </td>
+                          </tr>
+                          
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
 
+                  <hr>
+                  <div class="form-group row">
+                    <div class="col-xs-6" style="padding-right: 0px">
+                       <div class="input-group">
+                          <select class="form-control" name="newPaymentMethod" id="newPaymentMethod" required>
+                            
+                              <option value="">Select payment method</option>
+                              <option value="cash">Cash</option>
+                              <option value="CC">Credit Card</option>
+                              <option value="DC">Debit Card</option>
 
+                          </select>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-6" style="padding-left: 0px">
+                       <div class="input-group">
+                         <input type="number" class="form-control" name="newCodeTransition" id="newCodeTransition" placeholder="0" readonly required>
+                          <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                        </div>
+                    </div>
+                    <br>
+                  </div>
                  
                  </div>
-               </form>
+              
              </div>
+             <div class="box-footer">
+               <button type="submit" class="btn btn-primary pull-right">Save sale</button>
+             </div>
+           </form>
+
            </div>
         </div>
 
         <div class="col-lg-7 hidden-md hidden-sm hidden-xs">
-          <div class="box box-warning"></div>
+          <div class="box box-warning">
+            <div class="box-header with-border"></div>
+            <div class="box-body">
+               <table class="table table-bordered table-striped dt-responsive salesTable">
+                  
+                <thead>
+
+                   <tr>
+                     
+                     <th style="width:10px">#</th>
+                     <th>Image</th>
+                     <th style="width:30px">Code</th>
+                     <th>Description</th>
+                     <th>Stock</th>
+                     <th>Actions</th>
+
+                   </tr> 
+
+                </thead>
+
+              </table>
+            </div>
+          </div>
           
         </div>
 
